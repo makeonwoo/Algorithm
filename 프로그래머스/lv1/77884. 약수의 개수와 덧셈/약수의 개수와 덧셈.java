16d0b19b146098c481008fd7ddeea2366d9c5e5e
makeonwoo <79950124+makeonwoo@users.noreply.github.com>
2022-11-16 16:50:35 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.16 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        int count = 0;
+        for(int i = left; i<=right; i++){
+           for(int j = 1; j<=i;j++){
+               if(i%j ==0) count++;
+           } 
+            count -= 2;
+            if(count%2 ==0) answer+=i;
+            else answer-=i;
+            count=0;
+        }
+        return answer;
+    }
+}
