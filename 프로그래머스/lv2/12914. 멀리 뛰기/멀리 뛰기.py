3cb1e09a73cf9f8836a531a394274d1beea929ab
Ma Keon Woo <79950124+makeonwoo@users.noreply.github.com>
2022-12-02 00:31:37 +0900

    Update 멀리 뛰기.py

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -3,3 +3,12 @@ def solution(n):
         li.append((li[i-1]+li[i-2])%1234567)
     return li[n+1]
 li = [0,1,1]
+
+def t(n,a):#피보나치수인걸 알아낸 방법
+    if n == a:
+        return li.append(a)
+    elif n<a:
+        return
+    else:
+        t(n,a+1)
+        t(n,a+2)
